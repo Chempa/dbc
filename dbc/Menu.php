368fa67e9f63d4865b34f5c63c263c8a39041d98
francischempa <francis.chempa@icloud.com>
2020-12-25 13:50:27 +0000

    redir

--- dbc/Menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Include the function library
-require 'Include/Config.php';
-
-use ChurchCRM\Utils\RedirectUtils;
-
-RedirectUtils::Redirect('v2/dashboard');
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* This will give an error. Note the output
  * above, which is before the header() call */
-header('Location: ./dbc');
+header('Location: https://divinebaptist.000webhostapp.com/');
 exit;
 ?>
